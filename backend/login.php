a952f043e1b0d2ecb36c6d2ff78af6d046180e72
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2023-01-19 15:08:10 -0800

    merged nao's change

--- backend/login.php
+++ backend/login.php
@@ -1,58 +1,54 @@
-<?php  
-header('Access-Control-Allow-Origin: http://localhost:3000');
+<?php
+header('Access-Control-Allow-Origin: http://localhost:3002');
 header('Access-Control-Allow-Methods:POST');
 header('Access-Control-Allow-Header:Content-Type');
 header('Content-Type:application/json');
 include "./config.php";
-session_start();//session starts here 
-// $dbcon=mysqli_connect("localhost","root","");  
-// mysqli_select_db($dbcon,"crud_homestay");  
-  
+session_start();//session starts here
+// $dbcon=mysqli_connect("localhost","root","");
+// mysqli_select_db($dbcon,"crud_homestay");
+
 if($_SERVER['REQUEST_METHOD']=="POST"){
-  $id = $_POST['id'];
-  $pass = $_POST['pass'];
-  // $type = intval($_POST['type']);
-  $dbcon = new mysqli($serverName,$dbUser,$dbpass,$dbName);
-  if($dbcon->connect_error){
-      echo "connection failed ".$dbcon->connect_error;
-  }else{
-      
-      // $sql = "SELECT fname FROM user_tb WHERE email='Doe@mil.com'";
-      $sql = "SELECT * FROM tbl_student WHERE student_id='".$id."'";
-      // $sql = "INSERT INTO user_tb (fname,lname,email,pass,type) VALUES ('$fname','$lname','$email','$pass',$type)";
-      $result = $dbcon->query($sql);
-      if ($result->num_rows > 0) {
-      $row = $result->fetch_assoc();
-      // output data of each row
-      //print_r($pass);
-      //print_r($row['pass']);
-      if (password_verify($pass,$row['pass'])){ //($password,$hashedPassword)
-      //if ($id==$row['student_id'] && $pass==$row['pass']){
-                      echo '123';
-                      // header("Location: ".'http://localhost:3000/nopage');
-                  }else{
-                      echo "You don't exist";
-              }
-      } else {
-      echo "0 results";
-      }
+$id = $_POST['id'];
+$pass = $_POST['pass'];
+$type = $_POST['type'];
 
-      // $sql = "SELECT * FROM user_tb";
-      // print_r($sql);
-      // $result = $conn->query($sql);
+// $type = intval($_POST['type']);
+$dbcon = new mysqli($serverName,$dbUser,$dbpass,$dbName);
+if($dbcon->connect_error){
+echo "connection failed ".$dbcon->connect_error;
+}else{
+// $sql = "";
+switch($type){
+case 0:
+$sql = "SELECT * FROM tbl_owner WHERE owner_id='".$id."'";
+break;
 
-      // if($dbcon->query($sql)===TRUE){
-      // if ($result->num_rows > 0) {
-      //     $row = $result->fetch_assoc();
-      //         if (password_verify($row['pass'], $pass)){
-      //             echo "You exist";
-      //         }
-      //         echo "You don't exist";
-      //     }
-      // }else{
-      //     echo "Error in the login: ".$dbcon->error;
-      // }
-      $dbcon->close();
-  }
+case 1:
+$sql = "SELECT * FROM tbl_student WHERE student_id='".$id."'";
+break;
+
+case 2:
+$sql = "SELECT * FROM tbl_webadmin WHERE admin_id='".$id."'";
+break;
 }
-?>  
+
+$result = $dbcon->query($sql);
+if ($result->num_rows > 0) {
+$row = $result->fetch_assoc();
+
+if (password_verify($pass,$row['pass'])){
+echo json_encode($row);
+
+}else{
+echo 1 ;
+}
+} else {
+echo 0;
+}
+
+
+$dbcon->close();
+}
+}
+?>
--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,63 +1,129 @@
 import userSrv from "../Services/userSrv";
 import {useState} from "react";
+import cryptoJs from "crypto-js";
 import { useNavigate } from "react-router-dom";
 
 function Test(){
-  const [res,setRes] = useState('');
-  const navigate = useNavigate();
-  
-  const register = (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    // formData.append("fakeInput","fakevalue");
-    userSrv.register("login.php",formData)
-    .then(response=>{
-      setRes(response.data);
-      console.log(response);
-      if (response.data==123){
-          navigate('/nopage');
-      }
-    })
-    .catch(err=>{
-      console.log(err);
-    })
-    
-    
-  }
-    return(
-        <>
-        <h1>{res}</h1>
-        <div className="card mb-3">
-    <div className="row g-0 d-flex align-items-center">
-      <div className="col-lg-4 d-none d-lg-flex">
-        <img src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg" alt="Trendy Pants and Shoes"
-          className="w-100 rounded-t-5 rounded-tr-lg-0 rounded-bl-lg-5" />
-      </div>
-      <div className="col-lg-8">
-        <div className="card-body py-5 px-md-5">
+const [res,setRes] = useState('');
+const navigate = useNavigate();
 
-        <form onSubmit={register}>  
-     
-            <div className="form-outline mb-4">
-                <label className="form-label">User ID  </label>
-                <input className="form-control" placeholder="input id" name="id" type="text" autoFocus/>  
-            </div>
-   
-            <div className="form-outline mb-4">
-                <label className="form-label">Password  </label>
-                <input className="form-control" placeholder="Password" name="pass" type="password"/> 
-            </div>
+const enc = (data,key) =>{
+const encData = cryptoJs.AES.encrypt(data,key).toString();
+return encData;
+};
+
+const login = (event) => {
+event.preventDefault();
+const formData = new FormData(event.target);
+userSrv.register("login.php",formData)
+.then(response=>{
+console.log(response.data);
+
+if(response.data == 0 ){
+setRes('User not found');
+}else if(response.data == 1){
+setRes('Username / Password is not correct');
+}else{
+//login success
+sessionStorage.setItem("user", enc(formData.get("id"), "Hotdog"));
+navigate('/logged');
+}
+})
+.catch(err=>{
+console.log(err);
+})
+
+}
+
+//***STYLE START ****** */
+const [isHover, setIsHover] = useState(false);
+const [isHover2, setIsHover2] = useState(false);
+
+const handleMouseEnter = () => {
+setIsHover(true);
+};
+const handleMouseLeave = () => {
+setIsHover(false);
+};
+const handleMouseEnter2 = () => {
+setIsHover2(true);
+};
+const handleMouseLeave2 = () => {
+setIsHover2(false);
+};
+const stylebtn = {
+// display: "flex",
+color: isHover ? 'black' : 'white',
+backgroundColor: isHover ? 'lightblue' : 'green',
+padding: "0px",
+fontFamily: "Arial",
+cursor: 'pointer',
+width:'15vh',
+height:'4vh',
+marginTop:'3.3vh'
+};
+const stylebtn1 = {
+// display: "flex",
+width:'15vh',
+height:'4vh',
+backgroundColor: isHover2 ? 'white' : 'DodgerBlue',
+color: isHover2 ? 'black' : 'white',
+padding: "0px",
+fontFamily: "Arial",
+cursor: 'pointer',
+};
+//*****STYLE END****** */
+
+const logOut =()=>{
+setRes('logout');
+sessionStorage.clear();
+}
+return(
+<>
+<button onClick={()=>logOut()}>Logout</button>
+<h1>{res}</h1>
+<div className="card mb-3">
+<div className="row g-0 d-flex align-items-center">
+<div className="col-lg-4 d-none d-lg-flex">
+<img src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg" alt="Trendy Pants and Shoes"
+className="w-100 rounded-t-5 rounded-tr-lg-0 rounded-bl-lg-5" />
+</div>
+<div className="col-lg-8">
+<div className="card-body py-5 px-md-5" style={{display:'flex'}}>
+
+<form onSubmit={(e)=>login(e)} style ={{display:'flex', alignItems:'center'}}>
+
+<div className="form-outline mb-4">
+<label className="form-label">User ID </label>
+<input className="form-control" placeholder="input id" name="id" type="text" autoFocus/>
+</div>
+
+<div className="form-outline mb-4">
+<label className="form-label">Password </label>
+<input className="form-control" placeholder="Password" name="pass" type="password"/>
+</div>
+{/* riku added from here */}
+<div className="form-floating mb-3">
+{/* change name */}
+<select name="type">
+<option disabled>Choose your usertype</option>
+<option value="0">admin</option>
+<option value="1">owner</option>
+<option value="2">student</option>
+</select>
+<label htmlFor="role"></label>
+</div>
+{/* to here */}
 
 
-            <input className="btn btn-lg btn-success btn-block" type="submit" value="login" name="login" />  
-
-          </form>
-
-        </div>
-      </div>
-    </div>
-  </div>
-        </>
-    )
+<input style={stylebtn1} className="btn btn-lg btn-success btn-block" type="submit" value="login" name="login" onMouseEnter={handleMouseEnter2} onMouseLeave={handleMouseLeave2}/>
+</form>
+<Link style={stylebtn} className="btn btn-lg btn-success btn-block" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} to="/register">register</Link>
+</div>
+</div>
+</div>
+</div>
+</>
+)
 };
 export default Test;
